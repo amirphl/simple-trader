15643a93aa2349cab500aaf72cd0e150346a25cd
Amir <amirphl4@gmail.com>
2025-08-01 17:49:03 +0000

    Remove unused websocketmanager

--- internal/exchange/websocket.go
+++ internal/exchange/websocket.go
@@ -501,44 +501,6 @@ func (w *WallexTradeChannel) connectAndStream(ctx context.Context) error {
 	}
 }
 
-type WebsocketManager interface {
-	Connect() error
-	Disconnect() error
-	Subscribe(channel string, params map[string]any) error
-	Unsubscribe(channel string) error
-	IsConnected() bool
-	// Add more as needed for state management
-}
-
-type WallexWebsocketManager struct {
-	// TODO: Add fields for connection, state, etc.
-}
-
-func (w *WallexWebsocketManager) Connect() error {
-	// TODO: Implement connection logic
-	return nil
-}
-
-func (w *WallexWebsocketManager) Disconnect() error {
-	// TODO: Implement disconnect logic
-	return nil
-}
-
-func (w *WallexWebsocketManager) Subscribe(channel string, params map[string]any) error {
-	// TODO: Implement subscribe logic
-	return nil
-}
-
-func (w *WallexWebsocketManager) Unsubscribe(channel string) error {
-	// TODO: Implement unsubscribe logic
-	return nil
-}
-
-func (w *WallexWebsocketManager) IsConnected() bool {
-	// TODO: Implement connection state check
-	return false
-}
-
 // OrderBookEntry represents a single order book entry
 type OrderBookEntry struct {
 	Quantity float64 `json:"quantity"`
