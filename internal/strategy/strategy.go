2980d2c8eec11e49397f67fd293ad9741cd94a84
Amir <amirphl4@gmail.com>
2025-08-02 13:46:19 +0000

    Create strategy per symbol

--- internal/strategy/strategy.go
+++ internal/strategy/strategy.go
@@ -43,13 +43,13 @@ func New(cfg config.Config, storage db.Storage) []Strategy {
 	strats := []Strategy{}
 
 	for _, stratName := range cfg.Strategies {
-		var strat Strategy
-
 		switch stratName {
 		case "ema":
 			// strat = strategy.NewEMACrossoverStrategy(10, 30)
 		case "rsi":
-			strat = NewRSIStrategy("ADA-USDT", 14, 70, 30, storage) // TODO: Make it configutable.
+			for _, symbol := range cfg.Symbols {
+				strats = append(strats, NewRSIStrategy(symbol, 14, 70, 30, storage)) // TODO: Make it configutable.
+			}
 		case "macd":
 			// strat = strategy.NewMACDStrategy(12, 26, 9)
 		case "composite":
@@ -61,16 +61,20 @@ func New(cfg config.Config, storage db.Storage) []Strategy {
 		case "rsi-obos":
 			// strat = strategy.NewRSIObOsStrategy(14, 70, 30)
 		case "Engulfing Heikin Ashi":
-			strat = NewEngulfingHeikinAshi("DOGE-USDT", storage)
+			for _, symbol := range cfg.Symbols {
+				strats = append(strats, NewEngulfingHeikinAshi(symbol, storage))
+			}
 		case "Stochastic Heikin Ashi":
-			strat = NewStochasticHeikinAshi("DOGE-USDT", storage)
+			for _, symbol := range cfg.Symbols {
+				strats = append(strats, NewStochasticHeikinAshi(symbol, storage))
+			}
 		case "Four Stochastic Heikin Ashi":
-			strat = NewFourStochasticHeikinAshi("DOGE-USDT", storage)
+			for _, symbol := range cfg.Symbols {
+				strats = append(strats, NewFourStochasticHeikinAshi(symbol, storage))
+			}
 		default:
 			// strat = strategy.NewSMACrossoverStrategy(10, 30)
 		}
-
-		strats = append(strats, strat)
 	}
 
 	return strats
