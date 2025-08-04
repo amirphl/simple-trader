8a8ec74934470653558a2018f86bf7b3f40186f8
Amir <amirphl4@gmail.com>
2025-08-04 14:06:56 +0000

    feat(strategy/stochastic_heikin_ashi): add state struct for readability

---
+++ internal/strategy/stochastic_heikin_ashi/state.go
@@ -0,0 +1,12 @@
+// Package stochastic_heikin_ashi
+package stochastic_heikin_ashi
+
+import "github.com/amirphl/simple-trader/internal/strategy/state_machine"
+
+const (
+	// NoPositionState - No position is held, monitoring for entry signals
+	NoPositionState state_machine.State = "No Position"
+
+	// WaitingForExitSignal - Waiting for exit signal
+	WaitingForExitSignal state_machine.State = "Waiting for exit signal"
+)
